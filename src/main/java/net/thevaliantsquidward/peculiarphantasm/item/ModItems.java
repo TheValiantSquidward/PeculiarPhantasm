ea82fd170067a3f15135fc9b1b5b9d6ae1c0a992
TheValiantSquidward <thevaliantsquidward@gmail.com>
2025-03-14 14:29:03 -0400

    Good Egg

--- src/main/java/net/thevaliantsquidward/peculiarphantasm/item/ModItems.java
+++ src/main/java/net/thevaliantsquidward/peculiarphantasm/item/ModItems.java
@@ -25,6 +25,9 @@ public class ModItems {
     public static final RegistryObject<Item> BAD_EGG = ITEMS.register
             ("bad_egg", () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> TROODON_EGG = ITEMS.register
+            ("troodon_egg", () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
